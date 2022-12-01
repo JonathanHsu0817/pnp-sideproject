b82c46f8d21590fb6e2220f0d89d3c57b7483345
Jonathan0817 <00339028@email.ntou.edu.tw>
2022-12-01 16:35:26 +0800

    Update 2022-12-01T08:35:19.029Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,29 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+var indexSwiper = new Swiper(".index-swiper", {
+  loop: true,
+  speed: 500,
+  autoplay: {
+    delay: 3000
+  },
+  centeredSlides: true,
+  slidesPerView: 2,
+  breakpoints: {
+    768: {
+      slidesPerView: 3
+    },
+    1200: {
+      slidesPerView: 2.9
+    },
+    1400: {
+      slidesPerView: 2.5
+    }
+  },
+  spaceBetween: 24,
+  effect: 'slide',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
 });
 //# sourceMappingURL=all.js.map
