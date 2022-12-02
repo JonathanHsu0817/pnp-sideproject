85e7edc1197f3ecf0925e6d179ede3d52da9cd77
Jonathan0817 <00339028@email.ntou.edu.tw>
2022-12-03 05:07:35 +0800

    Update 2022-12-02T21:07:31.808Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,16 @@
 "use strict";
 
+var adminSwiper = new Swiper(".admin-swiper", {
+  slidesPerView: 4,
+  spaceBetween: 24,
+  freeMode: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
+});
+"use strict";
+
 var indexSwiper = new Swiper(".index-swiper", {
   loop: true,
   speed: 500,
