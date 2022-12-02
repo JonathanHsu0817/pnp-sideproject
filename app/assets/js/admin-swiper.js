1992d08866370f89976485094d5fccb2aaa12d9b
Jonathan0817 <00339028@email.ntou.edu.tw>
2022-12-03 05:06:39 +0800

    切版完後台出版

---
+++ app/assets/js/admin-swiper.js
@@ -0,0 +1,9 @@
+const adminSwiper = new Swiper(".admin-swiper", {
+    slidesPerView: 4,
+    spaceBetween: 24,
+    freeMode: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+  });
